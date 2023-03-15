067dedd16658b683cd557cee1e901d6b042e8f80
Sai Tejeswar Reddy Chintha <80686447+saitejeswar1@users.noreply.github.com>
2023-03-15 14:24:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title('Simple Streamlit App')
 st.markdown("""
 This app performs Word Cloud
 * **Python libraries:** streamlit, pandas, BeautifulSoup, wordcloud ......
-* **Need to cantact: ** [SeaportAI.com](https://seaportai.com/contact-us/).
+* **Need to cantact: ** [Sai Tejeswar](https://github.com/saitejeswar1).
 """)
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -20,11 +20,11 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 #if file_bytes is not None:
 st.sidebar.header("Select Link")
-links = ["https://seaportai.com/blog-predictive-maintenance/",
-        "https://seaportai.com/2019/04/22/healthcare-analytics/",
-        "https://seaportai.com/blog-rpameetsai/",
-        "https://seaportai.com/covid-19/",
-        "https://seaportai.com/industry4-0/"]
+links = ["https://annabyang.medium.com/whats-going-on-the-silicon-valley-bank-collapse-vs-the-2008-financial-crisis-8b5539995e63",
+        "https://annabyang.medium.com/why-writers-should-care-about-ai-art-b859192776c9",
+        "https://thisisyouth.medium.com/chatgpt-wont-kill-writing-but-it-will-kill-content-d3d2fe3af3cd",
+        "https://towardsdatascience.com/a-decade-of-knowledge-graphs-in-natural-language-processing-5fdb15abc2b3",
+        "https://towardsdatascience.com/beautifully-illustrated-nlp-models-from-rnn-to-transformer-80d69faf2109"]
 URL = st.sidebar.selectbox('Link', links)
 st.sidebar.header("Select No.of words you want to display")
 words = st.sidebar.selectbox("No.of Words", range(10,1000,10))
